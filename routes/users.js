1e9463d2d1af6fd5c7d38aa3169da532c5ac4488
Quoc Hung <quochung1997s@gmail.com>
2019-05-27 04:02:53 +0700

    done bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const {registerValidation} = require('../validation');
+const bcrypt = require('bcrypt');
 
 router.post('/register', async (req, res) => {
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password
+    password: bcrypt.hashSync(req.body.password, 10)
   });
 
   // check email
@@ -16,11 +16,26 @@ router.post('/register', async (req, res) => {
 
   try {
     const saveUser = await user.save();
+    saveUser.password = req.body.password;
     return res.send(saveUser);
   } catch (err) {
     return res.status(400).send(err.errmsg);
   }
 
+});
+
+router.post('/login', async (req, res) => {
+  const user = await User.findOne({email: req.body.email});
+
+  if (!user) return res.status(400).send('Email does not exist');
+
+  const checkPassword = bcrypt.compareSync(req.body.password, user.password);
+
+  user.password = req.body.password;
+
+  if (checkPassword) return res.send(user);
+  
+  return res.status(400).send('Invalid password');
 });
 
 module.exports = router;
